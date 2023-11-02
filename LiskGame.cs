b99b2b1845a5d6803a2ca23810856119c3548e9a
viylouu <honeefunnee@gmail.com>
2023-11-02 10:03:32 -0600

    tried to implement toronoi

--- LiskGame.cs
+++ LiskGame.cs
@@ -20,11 +20,13 @@ namespace viylouuInc_Launcher
 
         ITexture toronoiTest = null;
 
+        public Random r = new Random();
+
         public void Update()
         {
             if (!started)
             {
-                toronoiTest = genToronoi(1000, new Vector2(100, 100), Color.White, Color.Black);
+                toronoiTest = genToronoi(1000, new Vector2(128, 128), Color.White, Color.Black);
 
                 started = true;
             }
@@ -67,12 +69,32 @@ namespace viylouuInc_Launcher
         public ITexture genToronoi(int iterations, Vector2 size, Color bgcol, Color paintcol)
         {
             ITexture noiseTex = Graphics.CreateTexture((int)size.X, (int)size.Y, TextureOptions.None);
+            int[,] blendVals = new int[(int)size.X, (int)size.Y];
+
+
+            Vector2 currentPos = new Vector2(size.X / 2, size.Y / 2);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                if (currentPos.X >= 0 && currentPos.X <= size.X && currentPos.Y >= 0 && currentPos.Y <= size.Y)
+                {
+                    if (blendVals[(int)currentPos.X, (int)currentPos.Y] < 255)
+                    { blendVals[(int)currentPos.X, (int)currentPos.Y] += 1; }
+                }
+
+                int dir = r.Next(0, 7);
+
+                currentPos += Angle.ToVector(dir * 45 * (float.Pi / 180));
+
+                currentPos = new Vector2(currentPos.X % size.X, currentPos.Y % size.Y);
+            }
 
             for (int x = 0; x < size.X; x++)
             {
                 for (int y = 0; y < size.Y; y++)
                 {
-                    noiseTex.GetPixel(x, y) = bgcol;
+                    blendVals[x, y] = 0;
+                    noiseTex.GetPixel(x, y) = Color.Lerp(bgcol, paintcol, (float)blendVals[x, y] / 255f);
                 }
             }
 
