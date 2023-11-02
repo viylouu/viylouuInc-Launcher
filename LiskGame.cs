c27f2ca995509535131f520faa8df34be94b1c51
viylouu <honeefunnee@gmail.com>
2023-11-01 20:32:54 -0600

    stage 1 of toronoi implementation

--- LiskGame.cs
+++ LiskGame.cs
@@ -18,10 +18,14 @@ namespace viylouuInc_Launcher
 
         public bool men = false;
 
+        ITexture toronoiTest = null;
+
         public void Update()
         {
             if (!started)
             {
+                toronoiTest = genToronoi(1000, new Vector2(100, 100), Color.White, Color.Black);
+
                 started = true;
             }
             else
@@ -50,12 +54,30 @@ namespace viylouuInc_Launcher
                 ICanvas canv = Graphics.GetOutputCanvas();
 
                 canv.Clear(new Color(0, 0, 0));
+
+                canv.DrawTexture(toronoiTest, new Vector2(Window.Width / 2, Window.Height / 2), new Vector2(Window.Height, Window.Height), Alignment.Center);
             }
         }
 
         public float getAngle(Vector2 p1, Vector2 p2)
         {
             return (float)Math.Atan2(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+        public ITexture genToronoi(int iterations, Vector2 size, Color bgcol, Color paintcol)
+        {
+            ITexture noiseTex = Graphics.CreateTexture((int)size.X, (int)size.Y, TextureOptions.None);
+
+            for (int x = 0; x < size.X; x++)
+            {
+                for (int y = 0; y < size.Y; y++)
+                {
+                    noiseTex.GetPixel(x, y) = bgcol;
+                }
+            }
+
+            noiseTex.ApplyChanges();
+            return noiseTex;
         }
     }
 }
