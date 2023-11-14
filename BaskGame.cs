5c32b8ddc7d9b3ca911eacbc086d6710d8f0412d
viylouu <honeefunnee@gmail.com>
2023-11-14 16:01:55 -0700

    new game

---
+++ BaskGame.cs
@@ -0,0 +1,17 @@
+﻿using SimulationFramework.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viylouuInc_Launcher
+{
+    internal class BaskGame
+    {
+        public void Update()
+        {
+            ICanvas canv = Graphics.GetOutputCanvas();
+        }
+    }
+}
